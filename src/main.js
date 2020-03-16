fcc9140f9a882d3fb217a658b7f5561e2051aef0
张柯雨 <zhangkeyu@ZBMAC-VX5ZDHTD5.local>
2020-03-16 21:32:15 +0800

    [feature] 删除jad-pc依赖

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import 'jad-pc/dist/jad.css'
 import '@/assets/scss/reset.scss'
 import '@/assets/scss/common.scss'
 
@@ -6,12 +5,10 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import router from './router'
 import store from './store'
-import JadPc from 'jad-pc'
 import App from './App.vue'
 import $post from './utils/$post'
 
 Vue.use(VueResource)
-Vue.use(JadPc)
 
 Vue.prototype.$post = $post
 Vue.config.productionTip = false
