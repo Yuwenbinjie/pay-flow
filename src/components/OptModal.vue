5a5f868fbdf474edeb4659ff6c29f329f9c81521
Yqwerty <yy@MacBook-Pro.local>
2020-04-22 11:18:51 +0800

    [feature] 添加modal

---
+++ src/components/OptModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <j-modal :is-show="showOptModal" :is-right="true" @on-ok="saveOptModal" @on-cancel="cancelOpt">
+    <template slot="modal-body">
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+      <p>test</p>
+    </template>
+  </j-modal>
+</template>
+
+<script>
+export default {
+  name: "OptModal",
+  props: {
+    showOptModal: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    }
+  },
+  methods: {
+    cancelOpt() {
+      this.$emit("close-opt");
+    },
+    saveOptModal() {
+        this.$emit("close-opt");
+    //   this.$emit("save-opt");
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
--- src/views/QualificationMark.vue
+++ src/views/QualificationMark.vue
@@ -7,17 +7,35 @@
             alt="Vue logo"
             src="../assets/imgs/logo.png"
         >
+        <j-button @click="showOptModal">打开modal</j-button>
         <HelloWorld msg="Welcome to Your Vue.js App" />
+        <opt-modal
+            :show-opt-modal="isShowOptModal"
+            @close-opt="isShowOptModal = false"
+        >
+        </opt-modal>
     </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
+import OptModal from '@/components/OptModal.vue'
 
 export default {
     name: 'Home',
     components: {
-        HelloWorld
+        HelloWorld,
+        OptModal,
+    },
+    data() {
+        return {
+            isShowOptModal: false
+        } 
+    },
+    methods: {
+        showOptModal() {
+            this.isShowOptModal = true
+        }
     }
 }
 </script>
