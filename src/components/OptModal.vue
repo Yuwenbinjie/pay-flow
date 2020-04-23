81fecfa775a6448144ed5c2700e1ed606c9fc98f
yuanyang12 <yuanyang@jd.com>
2020-04-23 19:20:39 +0800

    [feature] 修改modal样式

--- src/components/OptModal.vue
+++ src/components/OptModal.vue
@@ -2,33 +2,96 @@
     <j-modal
         :is-show="showOptModal"
         :is-right="true"
+        :is-mask-closable="false"
+        :is-esc-closable="false"
         @on-ok="saveOptModal"
         @on-cancel="cancelOpt"
     >
+        <template slot="modal-title">
+            {{ title }}
+        </template>
         <template slot="modal-body">
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
+            <div class="mb48">
+                <span class="inline-block w120">资质类型<span class="c-red ml8">*</span></span>
+                <j-select
+                    class="w200"
+                    type="default"
+                    :options="qualificationOptions"
+                    v-model="params.type"
+                />
+            </div>
+            <div class="mb48">
+                <span class="inline-block w120">有效时间</span>
+                <j-date-picker
+                    class="inline-block"
+                    type="daterange"
+                    style="width: 200px"
+                    v-model="date"
+                    @on-change="selectedDate"
+                    :is-range-input="true"
+                    is-clearable
+                />
+            </div>
+            <div class="mb48">
+                <span class="inline-block w120">品牌商品名</span>
+                <j-input
+                    class="w200"
+                    v-model="params.brandName"
+                />
+            </div>
+            <div class="mb48">
+                <span class="vt inline-block w120">备注文案</span>
+                <j-input
+                    class="vt inline-block w400"
+                    type="textarea"
+                    is-autosize
+                    :rows="{min: 5}"
+                    v-model="wordList"
+                />
+            </div>
+            <div class="mb48">
+                <span class="inline-block w120">是否有效<span class="c-red ml8">*</span></span>
+                <j-select
+                    class="w200"
+                    type="default"
+                    :options="validOptions"
+                    v-model="params.isValid"
+                />
+            </div>
+            <div class="mb48">
+                <span class="inline-block w120">无效原因</span>
+                <j-input
+                    class="vt inline-block w400"
+                    type="textarea"
+                    is-autosize
+                    :rows="{min: 5}"
+                    v-model="params.invalidReason"
+                />
+            </div>
+        </template>
+        <template slot="modal-footer">
+            <j-button
+                size="medium"
+                class="full-right mr16"
+                @click="cancelOpt"
+            >
+                取消
+            </j-button>
+            <j-button
+                size="medium"
+                class="full-right mr16"
+                type="primary"
+                @click="saveOptModal"
+            >
+                确定
+            </j-button>
         </template>
     </j-modal>
 </template>
 
 <script>
+import {qualificationOptions, validOptions} from '@/constant/OptModal'
+
 export default {
     name: 'OptModal',
     props: {
@@ -39,12 +102,50 @@ export default {
             }
         }
     },
+    data() {
+        return {
+            date: '',
+            title: '123',
+            qualificationOptions: qualificationOptions,
+            validOptions: validOptions,
+            params: {
+                type: null,
+                brandName: '',
+                isValid: '',
+                markStartTime: '',
+                markEndTime: '',
+                invalidReason: '',
+            },
+        }
+    },
     methods: {
         cancelOpt() {
+            this.date = ''
+            this.params = {
+                type: null,
+                brandName: '',
+                isValid: '',
+                markStartTime: '',
+                markEndTime: '',
+                invalidReason: '',
+            },
             this.$emit('close-opt');
         },
         saveOptModal() {
+            this.params.markStartTime = this.date.start
+            this.params.markEndTime = this.date.end
+            console.log(this.params)
+            this.date = ''
+            this.params = {
+                type: null,
+                brandName: '',
+                isValid: '',
+                markStartTime: '',
+                markEndTime: '',
+                invalidReason: '',
+            }
             this.$emit('close-opt');
+
             //   this.$emit("save-opt");
         }
     }
---
+++ src/constant/OptModal.js
@@ -0,0 +1,83 @@
+export const qualificationOptions = [
+    {
+        label: '模特/手绘免责声明',
+        value: 1,
+    },
+    {
+        label: '首发证明',
+        value: 2,
+    },
+    {
+        value: 3,
+        label: '专利',
+    },
+    {
+        value: 4,
+        label: '明星肖像授权',
+    },
+    {
+        value: 5,
+        label: '品牌授权',
+    },
+    {
+        value: 6,
+        label: '广告审查表',
+    },
+    {
+        value: 7,
+        label: '特殊化妆品行政许可批件',
+    },
+    {
+        value: 8,
+        label: '质检报告',
+    },
+    {
+        value: 9,
+        label: '营业执照',
+    },
+    {
+        value: 10,
+        label: '进口报关单',
+    },
+    {
+        value: 11,
+        label: '出口报关单',
+    },
+    {
+        value: 12,
+        label: '数据认证报告',
+    },
+    {
+        value: 13,
+        label: '首发承诺函',
+    },
+    {
+        value: 14,
+        label: '活动立项证明',
+    },
+    {
+        value: 15,
+        label: '中华老字号',
+    },
+    {
+        value: 16,
+        label: '非物质文化遗产',
+    },
+    {
+        value: 17,
+        label: '央行批准文件',
+    },
+]
+
+export const validOptions = [
+    {
+        label: '有效',
+        value: 0,
+    }, {
+        label: '无效',
+        value: 1,
+    }, {
+        label: '其他',
+        value: 2,
+    }
+]
