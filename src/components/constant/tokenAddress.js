2f86d338aee47c8b64754ac4d16abe00df585c96
yuwenbinjie <yuwenbinjie@jd.com>
2020-09-15 18:42:22 +0800

    [optimization] 币种地址替换

--- src/components/constant/tokenAddress.js
+++ src/components/constant/tokenAddress.js
@@ -1,6 +1,6 @@
 export const tokenAddressOpt = [
     {
-        value: '0x26Ec63a4672AdD5509C8cbC1f1c88d5958D22408',
+        value: '0x62E9019a47Ea604c58139EF3D21852D6b18D469B',
         label: 'testDAI'
     },
 ]
