ac55298a648818a1554deadccdcc97146337615d
yuwenbinjie <yuwenbinjie@jd.com>
2020-09-21 22:02:18 +0800

    [optimization] 静态资源引入cdn

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import '@/assets/scss/reset.scss'
-import '@/assets/scss/common.scss'
-import '@/assets/scss/layout.scss'
-import '@/assets/scss/font.scss'
+// import '@/assets/scss/reset.scss'
+// import '@/assets/scss/common.scss'
+// import '@/assets/scss/layout.scss'
+// import '@/assets/scss/font.scss'
 
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import router from './router'
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 import store from './store'
 import App from './App.vue'
 import web3 from './utils/web3'
